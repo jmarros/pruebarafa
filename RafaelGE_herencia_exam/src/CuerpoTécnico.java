f5f823a7a622e2c8cd975fee4a3d6dce4ea2d640
Jesús <Jess@LAPTOP-7RF7MK5H>
2017-04-20 16:53:33 +0200

    versionJesúsMartín

--- RafaelGE_herencia_exam/src/CuerpoTécnico.java
+++ RafaelGE_herencia_exam/src/CuerpoTécnico.java
@@ -1,6 +1,17 @@
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CuerpoTécnico.
+ */
 public class CuerpoTécnico {
+ 
+ /** The idutillero. Declaro la variable idutillero */
  private int idutillero;
+ 
+ /** The idpreparador. Declaro la variable idutillero */
  private int idpreparador;
+ 
+ /** The idmasajista. Declaro la variable idutillero */
  private int idmasajista;
 }
+/* he incluido esta clase en el proyecto herencia_exam*/
